f5fefcacf5d0e8c3f10fcd258d5c38136a813b4b
Hannes Hochreiner <hannes@hochreiner.net>
2020-03-07 09:33:03 +0100

    split the index file

---
+++ src/binInfCo.js
@@ -0,0 +1,77 @@
+import { Client } from "ssh2";
+import { ContextSsh } from './contextSsh';
+import { default as Axios } from 'axios';
+import * as net from 'net';
+import * as fs from 'fs';
+import * as crypto from 'crypto';
+import { default as commander } from 'commander';
+import { InfCo } from './infCo';
+import { ValueTransformer } from './valueTransformer';
+import { TaskExec } from './taskExec';
+import { TaskRequest } from './taskRequest';
+import { TaskTransfer } from './taskTransfer';
+import { TaskCouchDb } from './taskCouchDb';
+import { TaskCouchDbDoc } from './taskCouchDbDoc';
+import { TaskDockerContainer } from './taskDockerContainer';
+import { TaskDockerNetwork } from './taskDockerNetwork';
+import { TaskGitHubDeployLatestRelease } from './taskGitHubDeployLatestRelease';
+
+commander.command('encrypt <value>')
+  .action(cmdEncrypt);
+commander.command('process')
+  .option('-h, --hosts <path>', 'list of hosts')
+  .option('-t, --tasks <path>', 'list of tasks')
+  .action(cmdProcess);
+commander.parse(process.argv);
+
+async function cmdEncrypt(value) {
+  try {
+    console.log(JSON.stringify(await valueTransformer.encrypt(value)));
+  } catch (error) {
+    console.log(error);
+  } finally {
+    process.exit();
+  }
+}
+
+async function cmdProcess(cmdObj) {
+  try {
+    let hosts = await readJson(cmdObj.hosts);
+    let tasks = await readJson(cmdObj.tasks);
+    let taskDict = {
+      exec: TaskExec,
+      request: TaskRequest,
+      transfer: TaskTransfer,
+      couchDb: TaskCouchDb,
+      couchDbDocument: TaskCouchDbDoc,
+      dockerContainer: TaskDockerContainer,
+      dockerNetwork: TaskDockerNetwork,
+      gitHubDeployLatestRelease: TaskGitHubDeployLatestRelease
+    };
+    let contextDict = {
+      contextSsh: new ContextSsh(new Client(), net, Axios)
+    };
+    const valueTransformer = new ValueTransformer(fs, crypto);
+
+    let infCo = new InfCo(valueTransformer, contextDict, taskDict);
+
+    await infCo.process(tasks, hosts);
+  } catch (error) {
+    console.log(error);    
+  } finally {
+    process.exit();
+  }
+}
+
+function readJson(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, (error, data) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+
+      resolve(JSON.parse(data));
+    });
+  });
+}
---
+++ src/infCo.js
@@ -0,0 +1,73 @@
+export class InfCo {
+  constructor(valueTransformer, contextDict, taskDict) {
+    this._valueTransformer = valueTransformer;
+    this._contextDict = contextDict;
+    this._taskDict = taskDict;
+  }
+
+  async process(tasks, hosts) {
+    let hostList = await this._transformValues(hosts);
+    let varList = await this._transformValues(tasks.vars || {});
+    this._valueTransformer.registerVars(varList);
+    let taskList = await this._transformValues(tasks.tasks);
+  
+    for (let hostIdx in hostList.hosts) {
+      await this._processHost(hostList.hosts[hostIdx], taskList);
+    }
+  }
+  
+  async _processHost(host, taskList) {
+    console.log(`===== ${host.title} =====`);
+
+    if (!this._contextDict.hasOwnProperty(host.context.type)) {
+      throw new Error(`Context "${host.context}" is unknown.`);
+    }
+    
+    let context = this._contextDict[host.context.type];
+  
+    try {
+      await context.open(host.context.config);
+  
+      for (let taskIdx in taskList) {
+        let task = taskList[taskIdx];
+  
+        if (!this._taskDict.hasOwnProperty(task.type)) {
+          throw new Error(`Task "${task.type}" is unknown.`);
+        }
+
+        console.log(`${task.title}: ${(await this._taskDict[task.type].run(context, task.config))}`);
+      }
+    } finally {
+      context.close();
+    }
+  }
+  
+  async _transformValues(obj) {
+    if (obj instanceof Array) {
+      for (let idx in obj) {
+        obj[idx] = await this._transformValues(obj[idx]);
+      }
+      return obj;
+    } else if (obj instanceof Object) {
+      let res = {};
+    
+      for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+          if (obj[key] instanceof Object) {
+            if (obj[key].valueTransform) {
+              res[key] = await this._valueTransformer.transform(obj[key]);
+            } else {
+              res[key] = await this._transformValues(obj[key]);
+            }
+          } else {
+            res[key] = obj[key];
+          }
+        }
+      }
+    
+      return res;
+    } else {
+      return obj;
+    }
+  }
+}
